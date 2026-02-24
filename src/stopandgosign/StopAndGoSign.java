9a1b99fa347bdf5319d84ea84d3dac6a5b8005d5
ST10508062 <ST10508062@myemeris.edu.za>
2026-02-24 12:35:06 +0200

    Fisrt Submission

---
+++ src/stopandgosign/StopAndGoSign.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stopandgosign;
+
+/**
+ *
+ * @author ST10508062
+ */
+public class StopAndGoSign {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      /*  System.out.println("        ***************      xxxxxxxxxxxxxxxxxx");
+        System.out.println("       *                *     x                 x");
+        System.out.println("      *       STOP       *   x         GO        x");
+        System.out.println("        *               *     x                 x");*/
+        System.out.println("         **************         xxxxxxxxxxxxxxxx");
+        System.out.println("                **                     xx ");
+        System.out.println("                **                     xx");
+        System.out.println("                **                     xx");
+        System.out.println("            *********              xxxxxxxxxx");
+        
+    }
+    
+}
